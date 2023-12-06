85df17d47555fd4ddc4d860b3caf225db0a7094b
ithink24 <2401ankitrajpoot@gmail.com>
2023-12-07 01:37:28 +0530

    redirect page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from './Pages/Login';
+import Dashboard from "./Pages/Dashboard";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route exact path="/" Component={Login}/>
+                    <Route exact path="/dashboard" Component={Dashboard}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import backGround from '../Images/background.jpeg'
-import studentLogin from '../Images/student.jpeg';
-import Profile from '../Images/profile.jpeg'
+import { useNavigate } from "react-router-dom";
+import backGround from '../../Images/background.jpeg'
+import studentLogin from '../../Images/student.jpeg';
+import Profile from '../../Images/profile.jpeg';
 import styles from './styles.module.css'
 
 function Login() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+    navigate("/dashboard");
+    }
+
   return (
     <div className={styles.container} style={{ backgroundImage: `url(${backGround})` }}>
         <div className={styles.sub_container}>
@@ -16,6 +23,7 @@ function Login() {
 
             <div className={styles.right_box}>
                 <img src={Profile} alt="profile" width={100} height={100}/>
+
                 <div className={styles.form}>
                     <div className={styles.form_heading}>Email Address:</div>
                     <input className={styles.input} type="text" placeholder='Type your email...'/>
@@ -23,7 +31,7 @@ function Login() {
                     <div className={styles.form_heading}>Password:</div>
                     <input className={styles.input} type="password" placeholder='Type your password...'/>
 
-                    <button className={styles.button}>Log in</button>
+                    <button className={styles.button} onClick={() => handleClick()}>Log in</button>                   
                 </div>
             </div>
       </div>
